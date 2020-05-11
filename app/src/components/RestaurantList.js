7dd4ed14078723d80e5a6d1d441013b5b1edd633
karan2141 <kvs2141@gmail.com>
2020-05-11 17:22:43 +0530

    integrated restaurant api with the listing menu

--- app/src/components/RestaurantList.js
+++ app/src/components/RestaurantList.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class RestaurantList extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Restaurant List</h1>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      list: null
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/restaurant").then(response => {
+      response.json().then(result => {
+        console.warn(result);
+        this.setState({ list: result });
+      });
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h1>Restaurant List</h1>
+        {this.state.list ? (
+          <div>
+            {this.state.list.map((item, i) => (
+              <div className="list-wrapper">
+                <span>{item.name}</span>
+                <span>{item.email}</span>
+                <span>{item.rating}</span>
+                <span>{item.address}</span>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Please Wait...</p>
+        )}
+      </div>
+    );
+  }
 }
